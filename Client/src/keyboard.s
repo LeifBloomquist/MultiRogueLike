77975b779ee38734a78c0717f4c4285b0e2af787
Leif Bloomquist <github@leifbloomquist.net>
2017-11-04 23:24:12 -0400

    Item pickup/drop works (single item)

--- Client/src/keyboard.s
+++ Client/src/keyboard.s
@@ -18,37 +18,51 @@ KEYSTART:
    jsr $FFE4    
    beq :-    ; No key pressed, return
 
-   cmp #87		;W - up
-   beq up
+   cmp #87		;w - north
+   beq north
+   
+   cmp #65		;a - west
+   beq west
+   
+   cmp #68		;d - east
+   beq east
 		
-   cmp #83		;s - down
-   beq down
-
-   cmp #65		;a - left
-   beq left
-
-   cmp #68		;d - right
-   beq right
+   cmp #88		;x - south
+   beq south
+   
+   cmp #43      ;+ Pick up
+   beq pickup
+   
+   cmp #45      ;- Drop
+   beq drop
    
    ; No match
    rts
 
-up:		
+north:		
    lda #ACTION_MOVE
    ldx #DIRECTION_NORTH
    jmp sendaction
 		
-down:	
+south:	
    lda #ACTION_MOVE
    ldx #DIRECTION_SOUTH
    jmp sendaction	
    
-left:	
+west:	
    lda #ACTION_MOVE
    ldx #DIRECTION_WEST
    jmp sendaction
 
-right:	
+east:	
    lda #ACTION_MOVE
    ldx #DIRECTION_EAST
+   jmp sendaction
+
+pickup:
+   lda #ACTION_PICKUP
+   jmp sendaction
+   
+drop:
+   lda #ACTION_DROP
    jmp sendaction
