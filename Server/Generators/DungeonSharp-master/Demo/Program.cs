325693e9090e2a7c7ae7ad9198c0960ab4943b92
Leif Bloomquist <github@leifbloomquist.net>
2017-12-29 11:50:45 -0500

    Added Cave Generators

---
+++ Server/Generators/DungeonSharp-master/Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿using DungeonSharp;
+
+namespace Demo
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Grid<Tile> tiles = Generator.Generate(50, 50, 20);
+            Renderer.Render(tiles);
+        }
+    }
+}
