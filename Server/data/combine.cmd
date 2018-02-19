b4a8cc0786b2dd54f68c496fff77081502631f94
Leif Bloomquist <github@leifbloomquist.net>
2018-02-19 16:41:02 -0500

    Moved client to $6000

--- Server/data/combine.cmd
+++ Server/data/combine.cmd
@@ -1,9 +1,9 @@
 @rem batch file to pad+combine all Rogue data for TFTP download
 
-@copy ..\..\Client\C64\src\rogue8000.bin .
+@copy ..\..\Client\C64\src\rogue6000.bin .
 
 trunc rogue-font.raw 2048
-trunc empty.raw      14336
-@rem leave rogue8000.bin as-is, since it's last
+trunc empty.raw      10240
+@rem leave rogue6000.bin as-is, since it's last
 
-copy /b /v /y rogue-font.raw+empty.raw+rogue8000.bin tftpboot\roguedata
+copy /b /v /y rogue-font.raw+empty.raw+rogue6000.bin tftpboot\roguedata
