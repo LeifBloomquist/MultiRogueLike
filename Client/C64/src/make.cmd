03f26e070914c587737e431996c9d509fd569c4b
Leif Bloomquist <github@leifbloomquist.net>
2018-02-19 17:41:30 -0500

    Map file

--- Client/C64/src/make.cmd
+++ Client/C64/src/make.cmd
@@ -4,7 +4,7 @@
 @del p:\rogue\*.bin
 
 @cc65\ca65 rogue.s
-@cc65\ld65 rogue.o -C cfg/c64prg-rogue-6000.cfg ip65/ip65/ip65.lib  ip65/drivers/c64rrnet.lib -o rogue6000.bin
+@cc65\ld65 rogue.o -C cfg/c64prg-rogue-6000.cfg ip65/ip65/ip65.lib  ip65/drivers/c64rrnet.lib -m rogue6000.map -vm -o rogue6000.bin
 copy /b /v loadaddress6000.bbb + rogue6000.bin p:\rogue\rogue6000-la.bin
 
 @dir p:\rogue\*.bin
