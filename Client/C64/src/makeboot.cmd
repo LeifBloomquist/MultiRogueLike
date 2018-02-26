1510bb5b3b38fd481533affb7e9726eb128b6d22
Leif Bloomquist <github@leifbloomquist.net>
2018-02-26 18:52:17 -0500

    Exomized Bootloader

--- Client/C64/src/makeboot.cmd
+++ Client/C64/src/makeboot.cmd
@@ -3,7 +3,9 @@
 @del *.prg
 
 @cc65\ca65 rogueboot.s
-@cc65\ld65 rogueboot.o -C cfg/c64prg-vortex-boot.cfg ip65/ip65/ip65.lib  ip65/drivers/c64rrnet.lib -o rogueboot.prg
+@cc65\ld65 rogueboot.o -C cfg/c64prg-vortex-boot.cfg ip65/ip65/ip65.lib  ip65/drivers/c64rrnet.lib -o rboottemp.prg
+
+@exo\exomizer.exe sfx basic rboottemp.prg -o rogueboot.prg
 
 @dir rogueboot.prg
-@copy rogueboot.prg p:\rogue\
+@copy rogueboot.prg p:\rogue\    
