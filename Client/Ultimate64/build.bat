69b120930b744640d71a5248eabe15e821be644a
Leif Bloomquist <github@leifbloomquist.net>
2020-03-28 18:58:03 -0400

    CFG file with no BASIC stub

--- Client/Ultimate64/build.bat
+++ Client/Ultimate64/build.bat
@@ -3,5 +3,6 @@
 
 rem path = C:\cc65\bin;%PATH%
 
+rem -C src\cfg\c64-nobasic.cfg
 cl65 -Oi -t c64 src\lib\ultimate_lib_net.c src\u-rogueclient.c ..\C64\src\defaultscreen.s ..\C64\src\colorlookup.s ..\C64\src\screen.s -o target\u-rogueclient.prg
 u64remote 192.168.7.64 load target\u-rogueclient.prg
