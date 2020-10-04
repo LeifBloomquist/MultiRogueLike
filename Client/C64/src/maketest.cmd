e109985dd5dddfd1bc02d1268f3e6d705619d8b8
Leif Bloomquist <github@leifbloomquist.net>
2020-10-04 14:33:15 -0400

    Fix dir path in make

--- Client/C64/src/maketest.cmd
+++ Client/C64/src/maketest.cmd
@@ -7,8 +7,8 @@
 @if not exist c:\rogue mkdir c:\rogue
 @del c:\rogue\*.bin
 
-@..\..\..\Tools\cc65\ca65 rogue.s
-@..\..\..\Tools\cc65\ld65 rogue.o -C cfg/c64prg-rogue-6000.cfg ip65/ip65/ip65.lib  ip65/drivers/c64rrnet.lib -m rogue6000.map -vm -o rogue6000.bin
+@..\..\..\Tools\cc65\bin\ca65 rogue.s
+@..\..\..\Tools\cc65\bin\ld65 rogue.o -C cfg/c64prg-rogue-6000.cfg ip65/ip65/ip65.lib  ip65/drivers/c64rrnet.lib -m rogue6000.map -vm -o rogue6000.bin
 @copy /b /v loadaddress6000.bbb + rogue6000.bin c:\rogue\rogue6000-la.bin
 
 @dir c:\rogue\*.bin
