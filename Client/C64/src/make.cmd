60a5510274ed79123406e2a6154d0235f7148286
Leif Bloomquist <github@leifbloomquist.net>
2020-10-04 13:39:57 -0400

    Tidy C64 make output

--- Client/C64/src/make.cmd
+++ Client/C64/src/make.cmd
@@ -9,12 +9,11 @@
 @..\..\..\Tools\cc65\bin\ld65 rogue.o -C cfg/c64prg-rogue-6000.cfg ip65/ip65/ip65.lib  ip65/drivers/c64rrnet.lib -m rogue6000.map -vm -o rogue6000.bin
 @copy /b /v loadaddress6000.bbb + rogue6000.bin ..\target\rogue6000-la.bin
 
-@dir ..\target\*.bin
+@dir /b ..\target\*.bin
 
-pause
 @rem Pad+combine all Rogue data for download.
 
-..\..\..\Tools\trunc ..\..\common\rogue-font.raw 2048
-..\..\..\Tools\trunc ..\..\common\empty.raw      6144
+@..\..\..\Tools\trunc ..\..\common\rogue-font.raw 2048
+@..\..\..\Tools\trunc ..\..\common\empty.raw      6144
 
-copy /b /v /y "..\..\common\rogue-font.raw"+"..\..\common\empty.raw"+rogue6000.bin ..\target\tftpboot\roguedata
+@copy /b /v /y "..\..\common\rogue-font.raw"+"..\..\common\empty.raw"+rogue6000.bin ..\target\tftpboot\roguedata
