293726d97dd38cee18527e173cd1a8ac9977a9fa
Leif Bloomquist <github@leifbloomquist.net>
2023-01-03 22:07:40 -0500

    Tidy up maps

--- Server/data/Tiled Test Maps/TileTest1.tsx
+++
@@ -1,2 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="TileTest1" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
--- Server/data/Tiled Test Maps/TileTest3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="TileTest3" tilewidth="8" tileheight="8" spacing="1" margin="1" tilecount="638" columns="29">
- <image source="../../Program Files/Tiled/examples/tmw_desert_spacing.png" width="265" height="199"/>
-</tileset>
